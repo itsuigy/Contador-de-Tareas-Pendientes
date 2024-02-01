87d0322fa46f9f317094099b77624ba52b151be2
itsuigy <pedravi.avi@gmail.com>
2024-01-31 23:51:43 -0600

    Cambios finales

--- script.js
+++ script.js
@@ -1,74 +1,85 @@
-let form = document.getElementById("form")
-let input = document.getElementById("input")
-let msg = document.getElementById("msg")
-let lista = document.getElementById("lista")
-let total = document.getElementById("total")
-let contadorTareas = 0;
-let data = {};
+document.addEventListener('DOMContentLoaded', () => {
+    recuperarEstado();
+});
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let lista = document.getElementById("lista");
+let total = document.getElementById("total");
+let contadorTareasTotales = 0;
+let contadorTareasCompletadas = 0;
+let contadorTareasEliminadas = 0;
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    formValidation()
-})
+    formValidation();
+});
 
 const formValidation = () => {
     if(input.value == ""){
-        input.style.border = "3px solid red"
+        msg.innerHTML = "La tarea no puede estar vacía.";
+        input.style.border = "3px solid red";
     } else {
-        msg.innerHTML = ""
-        input.style.border = "3px solid black"
-        acceptData()
+        msg.innerHTML = "";
+        input.style.border = "3px solid black";
+        acceptData();
     }
-}
+};
 
 const acceptData = () => {
-    data["text"] = input.value;
-    contadorTareas = contadorTareas + 1;
-    total.innerHTML = `Total de tareas: ${contadorTareas}`
-    crearTarea()
-}
+    crearTarea(input.value);
+    input.value = "";
+    contadorTareasTotales++;
+    actualizarContadores();
+};
 
-let crearTarea = () => {
-
-    lista.innerHTML += `
-    <div>
-        <p>${data.text}</p>
+const crearTarea = (tarea) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+        <p>${tarea}</p>
         <span class="options">
-            <i onClick="editarTarea(this)" class="fas fa-edit"></i>
             <i onClick="eliminarTarea(this)" class="fas fa-trash"></i>
             <i onClick="completarTarea(this)" class="fas fa-check"></i>
-        </span>
-    </div>
-`;
+        </span>`;
+    lista.appendChild(div);
+    guardarEstado();
+};
 
-input.value = "";
+const completarTarea = (element) => {
+    element.parentElement.parentElement.classList.toggle("completada");
+    contadorTareasCompletadas = document.querySelectorAll('.completada').length;
+    actualizarContadores();
+    guardarEstado();
+};
 
-}
+const eliminarTarea = (element) => {
+    element.parentElement.parentElement.remove();
+    contadorTareasEliminadas++;
+    contadorTareasTotales--;
+    actualizarContadores();
+    guardarEstado();
+};
 
-let completarTarea = function (element) {
-    let tarea = element.parentElement.parentElement;
-    tarea.classList.toggle("completada");
-    if (tarea.classList.contains('completada')) {
-        contadorTareas = contadorTareas - 1;
-        total.innerHTML = `Total de tareas: ${contadorTareas}`
-    } else {
-        contadorTareas = contadorTareas + 1;
-        total.innerHTML = `Total de tareas: ${contadorTareas}`
+const actualizarContadores = () => {
+    total.innerHTML = `Total de tareas: ${contadorTareasTotales}`;
+    document.getElementById('completadas').innerText = `Tareas Completadas: ${contadorTareasCompletadas}`;
+    document.getElementById('eliminadas').innerText = `Tareas Eliminadas: ${contadorTareasEliminadas}`;
+};
+
+const guardarEstado = () => {
+    localStorage.setItem('listaTareas', lista.innerHTML);
+    localStorage.setItem('contadorTareasTotales', contadorTareasTotales);
+    localStorage.setItem('contadorTareasEliminadas', contadorTareasEliminadas);
+    localStorage.setItem('contadorTareasCompletadas', contadorTareasCompletadas);
+};
+
+const recuperarEstado = () => {
+    if (localStorage.getItem('listaTareas')) {
+        lista.innerHTML = localStorage.getItem('listaTareas');
+        contadorTareasTotales = parseInt(localStorage.getItem('contadorTareasTotales'));
+        contadorTareasEliminadas = parseInt(localStorage.getItem('contadorTareasEliminadas'));
+        contadorTareasCompletadas = parseInt(localStorage.getItem('contadorTareasCompletadas'));
+        actualizarContadores();
     }
-}
-
-let eliminarTarea = function (element) {
-    let tarea = element.parentElement.parentElement;
-    contadorTareas = contadorTareas - 1;
-    total.innerHTML = `Total de tareas: ${contadorTareas}`
-    tarea.remove();
-}
-
-const editarTarea = (element) => {
-    let tarea = element.parentElement.parentElement;
-    contadorTareas = contadorTareas - 1;
-    total.innerHTML = `Total de tareas: ${contadorTareas}`
-    
-    input.value = tarea.children[0].innerHTML;
-    tarea.remove();
-}
+};
